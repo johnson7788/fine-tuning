112cc32457356baeea3dc7167edf47a2a62804aa
johnson7788 <linghuchongxajh@gmail.com>
2020-09-22 09:42:01 +0800

    add files

--- code/yolo/predict.py
+++ code/yolo/predict.py
@@ -1,69 +1,99 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# Download YOLO necessary files.
-
-# Initialize parameters.
-
-# In[1]:
-
-
-confThreshold = 0.25  #Confidence threshold
-nmsThreshold = 0.45   #Non-maximum suppression threshold
-
-inpWidth = 416      #Width of network's input image
-inpHeight = 416       #Height of network's input image
-
-
-# Import libraries and define auxiliary functions.
-
-# In[2]:
-
+# 导入库并定义辅助功能。 define auxiliary functions.
 import matplotlib.pyplot as plt
 import cv2 as cv
 import numpy as np
+import imutils
+
+######################################################
+# 下载和准备好YOLO的必要文件，初始化参数
+######################################################
+
+# 置信度阈值
+confThreshold = 0.25
+# 非最大抑制阈值, Non-maximum suppression threshold, NMS
+nmsThreshold = 0.45
+
+# 网络输入图像的宽度, Width of network's input image
+inpWidth = 416
+
+# 网络输入图像的高度, Height of network's input image
+inpHeight = 416
+
+# 加载模型和类。
+# 加载类别名称，目前只有一个类别，table
+classesFile = "vocTablas.names";
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+# 给出模型的配置文件和权重文件，并使用它们加载网络。
+modelConfiguration = "tablasFinaltest416320.cfg";
+modelWeights = "tablasFinaltrain_10000.weights"
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
 
-# In[3]:
-
-
-# Get the names of the output layers
 def getOutputsNames(net):
-    # Get the names of all the layers in the network
+    """
+    获取输出层的名称
+    :param net:
+    :return:
+    """
+    # 获取网络中所有层的名称
     layersNames = net.getLayerNames()
-    #for i in net.getUnconnectedOutLayers():
+    # for i in net.getUnconnectedOutLayers():
     #    print(i[0]-1)
-    # Get the names of the output layers, i.e. the layers with unconnected outputs
-    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    # 获取输出图层的名称，即the layers with unconnected outputs, ['yolo_82', 'yolo_94', 'yolo_106']
+    layers = [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    return layers
 
-# Draw the predicted bounding box
-def drawPred(frame,classId, conf, left, top, right, bottom):
-    # Draw a bounding box.
+
+def drawPred(frame, classId, conf, left, top, right, bottom):
+    """
+    绘制预测的边界框,  bounding box
+    :param frame:
+    :param classId:
+    :param conf:
+    :param left:
+    :param top:
+    :param right:
+    :param bottom:
+    :return:
+    """
     cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
-    
+    #置信度
     label = '%.2f' % conf
-        
-    # Get the label for the class name and its confidence
+
+    # 获取类别名称及其可信度的标签
     if classes:
-        assert(classId < len(classes))
+        assert (classId < len(classes))
+        #label变成 'table:0.99'
         label = '%s:%s' % (classes[classId], label)
 
-    #Display the label at the top of the bounding box
-    labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    # 在边框上方显示标签
+    labelSize, baseLine = cv.getTextSize(text=label, fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=0.5, thickness=1)
     top = max(top, labelSize[1])
-    #cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine), (255, 255, 255), cv.FILLED)
-    cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 1)
+    # cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine), (255, 255, 255), cv.FILLED)
+    cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
 
-# Remove the bounding boxes with low confidence using non-maxima suppression
+
 def postprocess(frame, outs):
+    """
+    使用非极大值抑制和最低置信度， 删除边界框
+    :param frame:
+    :param outs:
+    :return:
+    """
+    # 获取图片的实际尺寸， 792 x 612
     frameHeight = frame.shape[0]
     frameWidth = frame.shape[1]
 
-    classIds = []
-    confidences = []
-    boxes = []
-    # Scan through all the bounding boxes output from the network and keep only the
-    # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+    # 扫描从网络输出的所有边界框，并仅保留具有高置信度得分的边界框。
+    # 将box's类别标签分配为得分最高的类别。
     classIds = []
     confidences = []
     boxes = []
@@ -72,6 +102,7 @@ def postprocess(frame, outs):
             scores = detection[5:]
             classId = np.argmax(scores)
             confidence = scores[classId]
+            #如果大于最低置信度的阈值，才保留
             if confidence > confThreshold:
                 center_x = int(detection[0] * frameWidth)
                 center_y = int(detection[1] * frameHeight)
@@ -83,9 +114,8 @@ def postprocess(frame, outs):
                 confidences.append(float(confidence))
                 boxes.append([left, top, width, height])
 
-    # Perform non maximum suppression to eliminate redundant overlapping boxes with
-    # lower confidences.
-    indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    # 执行NMS非极大抑制，以消除具有较低置信度的冗余重叠框,最终画出一个边界框
+    indices = cv.dnn.NMSBoxes(bboxes=boxes, scores=confidences, score_threshold=confThreshold, nms_threshold=nmsThreshold)
     for i in indices:
         i = i[0]
         box = boxes[i]
@@ -93,94 +123,60 @@ def postprocess(frame, outs):
         top = box[1]
         width = box[2]
         height = box[3]
-        drawPred(frame,classIds[i], confidences[i], left, top, left + width, top + height)
+        drawPred(frame, classIds[i], confidences[i], left, top, left + width, top + height)
     return boxes
 
 
-# Load the model and classes. 
 
-# In[4]:
-
-
-# Load names of classes
-classesFile = "vocTablas.names";
-classes = None
-with open(classesFile, 'rt') as f:
-    classes = f.read().rstrip('\n').split('\n')
- 
-# Give the configuration and weight files for the model and 
-# load the network using them.
-modelConfiguration = "tablasFinaltest416320.cfg";
-modelWeights = "tablasFinaltrain_10000.weights"
-
-net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
-
-
-# Read the input for the case of an image. 
-
-# In[5]:
-
-
-
-
-
-# The input image to a neural network needs to be in a certain format called a blob.
-# 
-# After a frame is read from the input image or video stream, it is passed through the blobFromImage function to convert it to an input blob for the neural network. In this process, it scales the image pixel values to a target range of 0 to 1 using a scale factor of 1/255. It also resizes the image to the given size of (416, 416) without cropping. Note that we do not perform any mean subtraction here, hence pass [0,0,0] to the mean parameter of the function and keep the swapRB parameter to its default value of 1.
-# 
-# The output blob is then passed in to the network as its input and a forward pass is run to get a list of predicted bounding boxes as the network’s output. These boxes go through a post-processing step in order to filter out the ones with low confidence scores. We will go through the post-processing step in more detail in the next section. We print out the inference time for each frame at the top left. The image with the final bounding boxes is then saved to the disk, either as an image for an image input or using a video writer for the input video stream.
-
-# In[6]:
-
-
-import imutils
-
-
-# In[7]:
-
-
-
-
-
-# In[8]:
-
-
-# Create a 4D blob from a frame.
-#frame = cv.resize(frame,(inpWidth, inpHeight))
+# 从框架创建4D blob。
+# frame = cv.resize(frame,(inpWidth, inpHeight))
 def predict(name):
+    """
+    预测图像
+    # 读取图像的输入。
+    # 神经网络的输入图像必须采用称为blob的某种格式。
+    # 从输入图像或视频流中读取帧后，将其通过blobFromImage函数传递，
+    # 以将其转换为神经网络的输入blob。
+    # 在此过程中，它使用1/255的缩放因子将图像像素值缩放到0到1的目标范围。
+    # 它还可以将图像调整为给定大小（416，416），而不会裁切。
+    # 请注意，这里我们不执行任何均值减法，因此将[0,0,0]传递给函数的均值参数，并将swapRB参数保持为其默认值1。
+    # 将输出Blob作为其输入传递到网络中，并运行前向传递以获取预测的边界框列表作为网络的输出。
+    # 这些框经过后处理步骤，以过滤掉置信度得分低的框。
+    # 我们在左上角打印出每个帧的推理时间。
+    # 然后将带有最终边界框的图像保存为磁盘，或者作为用于图像输入的图像，或者使用视频写入器作为输入视频流。
+    :param name:
+    :return: 返回bounding box 的坐标位置
+    """
+    #读取图像
     frame = cv.imread(name)
-    blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+    # blob 归一化， [batch_size, RGB_channel:3, W, H]
+    blob = cv.dnn.blobFromImage(image=frame, scalefactor=1 / 255, size=(inpWidth, inpHeight), mean=[0, 0, 0], swapRB=1, crop=False)
 
-    # Sets the input to the network
+    # 设置网络输入
     net.setInput(blob)
 
-    # Runs the forward pass to get output of the output layers
+    # 运行前向传递以获取输出层的输出
     outs = net.forward(getOutputsNames(net))
 
-    # Remove the bounding boxes with low confidence
-    boxes1=postprocess(frame, outs)
-    # Put efficiency information. The function getPerfProfile returns the 
-    # overall time for inference(t) and the timings for each of the layers(in layersTimes)
+    # 移除低置信度的边界框,
+    boxes1 = postprocess(frame, outs)
+    # 放入efficiency信息。 函数getPerfProfile返回推理的总时间（t）和每个图层的计时（以layersTimes为单位）
     t, _ = net.getPerfProfile()
-    #label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
-    #cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-    cv.imwrite("prediction.jpg",frame)
+    # label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+    # cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    #画出预测的bounding box
+    cv.imwrite("prediction.jpg", frame)
     return boxes1
 
 
-# In[ ]:
-
-
 def showImage(image):
-    if len(image.shape)==3:
-        img2 = image[:,:,::-1]
+    if len(image.shape) == 3:
+        img2 = image[:, :, ::-1]
         plt.imshow(img2)
         plt.show()
     else:
         img2 = image
-        plt.imshow(img2,cmap='gray')
+        plt.imshow(img2, cmap='gray')
         plt.show()
 
 
